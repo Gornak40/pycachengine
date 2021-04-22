006d24d83f2c59270113ab836ce7933343ee3bb5
Gornak40 <s-kozelsk@yandex.ru>
2021-04-22 11:06:16 +0300

    working caching now

---
+++ db-viewer.py
@@ -0,0 +1,9 @@
+#!venv/bin/python
+from unqlite import UnQLite
+
+
+db_path = 'pycachengine.db'
+db = UnQLite(db_path)
+for key, value in db.items():
+	print(f'{key}: {eval(value.decode("utf-8"))}')
+db.close()
--- pycachengine.py
+++ pycachengine.py
@@ -2,17 +2,22 @@
 from subprocess import Popen, PIPE
 from chess import Board
 from os import system
+from unqlite import UnQLite
 
 
 class PyCachEngine:
-	def __init__(self, path):
-		self.engine = Popen(path, universal_newlines=True, stdin=PIPE, stdout=PIPE)
+	def __init__(self, path, db_path, options=dict()):
 		self.board = Board()
+		self.db = UnQLite(db_path)
+		self.engine = Popen(path, universal_newlines=True, stdin=PIPE, stdout=PIPE)
 		self._put('uci')
 		self._ready()
-		self.learn(1000)
+		for option, val in options.items():
+			self._set_option(option, val)
+		self.learn(100)
 
 	def __del__(self):
+		self.db.close()
 		self.engine.kill()
 
 	def _put(self, line):
@@ -34,22 +39,55 @@ class PyCachEngine:
 	def _bestmove(self):
 		while True:
 			line = self._read()
+			if 'depth' in line:
+				depth = int(line.split()[2])
 			if 'bestmove' in line:
-				return line.split()[1]
+				move = line.split()[1]
+				return (move, depth)
+
+	def _set_option(self, option, value):
+		self._put(f'setoption option {option} value {value}')
+
+	def _store(self, new_fen, move, depth):
+		with self.db.transaction():
+			if new_fen in self.db:
+				_move, _depth = eval(self.db[new_fen].decode('utf-8'))
+				print(_move, _depth)
+				if int(_depth) >= depth:
+					return
+			self.db[new_fen] = (move, depth)
+		self.db.commit()
 
 	def learn(self, movetime):
-		self._put(f'position fen {self.board.fen()}')
+		fen = self.board.fen()
+		new_fen = ' '.join(fen.split()[:-2])
+		self._put(f'position fen {fen}')
 		self._put(f'go movetime {movetime}')
-		move = self._bestmove()
+		move, depth = self._bestmove()
 		self.board.push_uci(move)
+		self._store(new_fen, move, depth)
 		system('clear')
+#		print(fen)
 		print(self.board)
+		print('new_fen:', new_fen)
+		print('depth:', depth)
+		print('move:', move)
+		print('db_size:', len(self.db))
 		if not self.board.is_game_over():
 			self.learn(movetime)
+		else:
+			print(self.board.outcome().result())
 
 
 if __name__ == '__main__':
 	try:
-		PyCachEngine('./stockfish_13_linux_x64_avx2')
+		options = {
+			'Threads': 4,
+			'Hash': 4096
+		}
+		PyCachEngine(
+			path='./stockfish_13_linux_x64_avx2', 
+			db_path='pycachengine.db',
+			options=options)
 	except KeyboardInterrupt:
 		exit()
